fd802b1fd6d623f8be966704295480a6a6e1c624
origin <mykhailo.pylka@gmail.com>
2020-09-24 19:07:17 +0300

    start idea

--- Cossacks/js/main.js
+++ Cossacks/js/main.js
@@ -1,3 +1,26 @@
-$(function(){
+// Cossacs -------------------------------------------
 
+$(document).ready(function () {
+  $(".header-slider").slick({
+    arrows: false,
+    draggable: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true, // режим плавної зміни слайдів
+  });
 });
+
+// анімація lotti
+var elem = document.getElementById("header-animation");
+var animData = {
+  container: elem,
+  renderer: "svg",
+  loop: false,
+  autoplay: true,
+  path: "../js/cossacs-1.json",
+};
+anim = lottie.loadAnimation(animData);
